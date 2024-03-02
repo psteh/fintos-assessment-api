a49c4c635d8b3de65f7c63d182febf4c5aba4a42
psteh <42758493+psteh@users.noreply.github.com>
2024-03-03 03:47:56 +0800

    [WIP] fix CORS and search

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const path = require('node:path');
 const AutoLoad = require('@fastify/autoload');
+const cors = require('@fastify/cors');
 
 require('dotenv').config();
 
@@ -34,6 +35,10 @@ module.exports = async function (fastify, opts) {
     // the default value is false
     forceClose: true,
     url: `${process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017'}/${process.env.DATABASE_NAME || 'fintos'}`,
+  });
+
+  fastify.register(cors, {
+    origin: process.env.WEB_URL || 'http://localhost:3000',
   });
 };
 
--- controllers/Tasks.js
+++ controllers/Tasks.js
@@ -10,19 +10,24 @@ const COLLECTION_NAME = 'tasks';
  * - collection name should be put in middleware
  * - convert to typescript
  * - add validation schema
+ * - improve API route to /api/*
+ * - add checking if due date is within 7 date (DUE_SOON status if Due date is within 7 days)
  */
 
 async function getAllTasks(request, reply) {
   const collection = this.mongo.db.collection(COLLECTION_NAME);
   try {
-    const page = request.query.page || 1;
+    const page = parseInt(request.query.page) || 1;
     const pageSize = Number(request.query.pageSize) || 10;
-    const search = request.query.search || null;
+    const search = request.query.search.length > 0 ? request.query.search : null;
     const skip = (page - 1) * pageSize;
-    const query = search ? { name: search, deletedAt: { $ne: null } } : { deletedAt: { $ne: null } };
+    const query = search ? { name: search, deletedAt: null } : { deletedAt: null };
 
-    const res = await collection.find(query).skip(skip).limit(pageSize).toArray();
-    return reply.code(200).send(res);
+    const totalDocuments = await collection.countDocuments();
+    const rows = await collection.find(query).skip(skip).limit(pageSize).toArray();
+    const totalPages = Math.ceil(totalDocuments / pageSize);
+
+    return reply.code(200).send({ page, rows, totalPages });
   } catch (err) {
     request.log.error(err.message);
     return reply.code(500).send({ message: err.message });
@@ -69,7 +74,6 @@ async function createTask(request, reply) {
 async function updateTask(request, reply) {
   const collection = this.mongo.db.collection(COLLECTION_NAME);
   try {
-    console.log(request.params);
     const id = new ObjectId(request.params.id);
     const task = await collection.findOne({ _id: id, deletedAt: null }, { _id: 0 });
 
@@ -85,7 +89,7 @@ async function updateTask(request, reply) {
           name: request.body.name,
           description: request.body.description,
           dueDate: request.body.dueDate,
-          status: request.body.status,
+          status: request.body.status || task.status,
           updatedAt: moment().toISOString(),
         },
       },
