8b20bb99c70492570273d6352b1ccfaea3da9ced
psteh <42758493+psteh@users.noreply.github.com>
2024-03-03 16:59:53 +0800

    add validation schema
    - add validation schema
    - general improvements

---
+++ controllers/constants.js
@@ -0,0 +1,12 @@
+const STATUS_MAP = {
+  NOT_URGENT: 'NOT_URGENT',
+  OVERDUE: 'OVERDUE',
+  DUE_SOON: 'DUE_SOON',
+};
+
+const DUE_SOON_DAYS = 7;
+
+module.exports = {
+  STATUS_MAP,
+  DUE_SOON_DAYS,
+};
